d0f00d077879dbe2e826df6794ff79a5b836aadd
Lucas Schweickert <cyberfly@gmx.net>
2026-06-25 14:27:22 +0200

    refactoring

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -13,7 +13,7 @@ func handlerLogs() func(routing.GameLog) pubsub.Acktype {
 		defer fmt.Print("> ")
 		err := gamelogic.WriteLog(gamelog)
 		if err != nil {
-			fmt.Printf("Could not write log to disk: %w", err)
+			fmt.Printf("Could not write log to disk: %v", err)
 			return pubsub.NackDiscard
 		}
 		return pubsub.Ack
